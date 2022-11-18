f5d0e3a61ea949cae2c9cb4e35c9fcc2d058c1d0
Shanjai-0811 <118507480+Shanjai-0811@users.noreply.github.com>
2022-11-18 12:05:34 +0530

    Add files via upload

---
+++ assesment-1.py
@@ -0,0 +1,27 @@
+#1- Create a login function
+print("que 1")
+Username = input ("Please enter your Username:")
+Password = input ("Please enter your Password:")
+if (Username == "shanjai" and Password == "Shanjai1234"):
+   print ("Welcome Shanjai")
+else:
+   print ( "Please check your Username and password")
+
+
+
+#2-  Write A Program to Find Even And Odd Numbers
+print("")
+print("que 2")
+number=int(input("Enter the value:"))
+if (number%2)==0:
+           print ("The number you have entered is a even number")
+else:
+   print("The number you have enterd is an odd number")
+
+
+#3- Create A List Of Fruits And Iterate Through Them
+print("")
+print("que 3")
+Fruits = ("apple","orange", "grapes","mango","dragon fruit", "muskmelon", "butterfruit")
+for x in Fruits:
+     print(x)
